0dfb158fdec32f41007b42529aae9990b5ee2131
Jeksonic <jeksonic@gmail.com>
2013-11-11 21:12:45 +0200

    Test Commit

--- src/com/example/smile/MainActivity.java
+++ src/com/example/smile/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Let the magic begin
 	}
 
 	@Override
