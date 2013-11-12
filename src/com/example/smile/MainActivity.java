aa35da61dbda1bfd91e4d5bd4a706bf94ec05323
Bilovus Dmytro <digi.te.ua@gmail.com>
2013-11-12 18:15:39 +0200

    added XMLParser.java, jokes.xml, edited MainActivity.java

--- src/com/example/smile/MainActivity.java
+++ src/com/example/smile/MainActivity.java
@@ -1,10 +1,28 @@
 package com.example.smile;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
 import android.os.Bundle;
+import android.renderscript.Element;
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	static final String PATH = "jokes.xml";
+    // XML node keys
+    static final String KEY_ITEM = "item"; // parent node
+    static final String KEY_ID = "id";
+    static final String KEY_NAME = "name";    
+    static final String KEY_DESC = "description";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +32,32 @@ public class MainActivity extends Activity {
 		// Let the magic begin
 		// double trouble
 		//coment
+		
+		final TextView txt_test = (TextView) findViewById(R.id.txt);
+		final Button button_info = (Button) findViewById(R.id.btn_next);
+		
+		button_info.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	XMLParser parser = new XMLParser();		
+        		AssetManager manager = getAssets();
+        		InputStream stream;
+        		try{
+        			stream = manager.open(PATH);		
+        			Document doc = parser.getDocument(stream);			
+        			
+        	        NodeList nl = doc.getElementsByTagName(KEY_ITEM);			
+        	        Element e = (Element) nl.item(1);
+        	        String joke = parser.getValue(e, KEY_DESC);
+        	                	        
+        	        txt_test.setText(joke);
+        	        
+        		}catch(IOException e1){txt_test.setText("testing not good " + e1);}
+            	
+            }
+        });
+		
+		
+                
 	}
 
 	@Override
